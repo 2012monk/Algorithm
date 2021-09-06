6de6b4dd700bb8ad39fd256898c97c30839e012e
monk <2012.monk@gmail.com>
2021-09-06 15:07:10 +0900

    BOJ#4179 불!

--- algo-java/src/BOJ/N4179불/Main.java
+++ algo-java/src/BOJ/N4179불/Main.java
@@ -1,4 +1,4 @@
-package BOJ.N4179탈출;
+package BOJ.N4179불;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,16 +14,32 @@ public class Main {
     static int n, m, dx, dy;
     static int[][] room;
     static Queue<Pos> q;
-    static Pos st;
+    static Pos cur;
     static int[][] dt = {{0,1},{0,-1},{1,0},{-1,0}};
 
     public static void main(String[] args) throws IOException {
         init();
         while (!q.isEmpty()) {
-            st = q.poll();
+            cur = q.poll();
             for (int[] t : dt) {
-                dx = st.x+t[0];
-                dy = st.y+t[1];
+                dx = cur.x+t[0];
+                dy = cur.y+t[1];
+                if (cur.d == -2 && dx >= 0 && dx < n && dy >= 0 && dy < m && room[dx][dy] >= 0) {
+                    room[dx][dy] = -2;
+                    q.offer(new Pos(dx,dy,-2));
+                }
+                if (cur.d > 0){
+                    if (dx >= 0 && dx < n && dy >= 0 && dy < m) {
+                        if (room[dx][dy] == 0) {
+                            room[dx][dy] = cur.d + 1;
+                            q.offer(new Pos(dx,dy,room[dx][dy]));
+                        }
+                    }
+                    else{
+                        System.out.println(cur.d);
+                        System.exit(0);
+                    }
+                }
             }
         }
         System.out.println("IMPOSSIBLE");
@@ -41,7 +57,7 @@ public class Main {
                 if (s.charAt(j) == '#') room[i][j] = -1;
                 if (s.charAt(j) == 'J') {
                     room[i][j]=1;
-                    st=new Pos(i,j,1);
+                    cur =new Pos(i,j,1);
                 }
                 if (s.charAt(j) == 'F') {
                     room[i][j]=-2;
@@ -49,7 +65,8 @@ public class Main {
                 }
             }
         }
-        q.offer(st);
+        q.offer(cur);
+        cur = null;
     }
 
     static class Pos {
@@ -59,7 +76,7 @@ public class Main {
         public Pos(int i, int j,int di) {
             x=i;
             y=j;
-            x=di;
+            d=di;
         }
     }
 }
