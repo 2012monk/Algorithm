553945d71e34368ac38b91ea6818d6cc950c1c41
monk <2012.monk@gmail.com>
2021-09-12 16:56:53 +0900

    BOJ#2749 피보나치의 수 3

---
+++ algo-py/baekjoon/2749#피보나치의수3/main.py
@@ -0,0 +1,25 @@
+MOD = 1000000
+
+
+def mat_mul(mat1, mat2, size=2):
+    tmp = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                tmp[i][j] += (mat1[i][k] * mat2[k][j]) % MOD
+    return tmp
+
+
+def power(n):
+    mat = [[1, 1], [1, 0]]
+    res = [[1, 1], [1, 0]]
+    while n > 0:
+        if n & 1:
+            res = mat_mul(mat, res)
+        mat = mat_mul(mat, mat)
+        n //= 2
+    return res
+
+
+if __name__ == '__main__':
+    print(power(int(input()) - 2)[0][0] % MOD)
