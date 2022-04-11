2b8426c4af0e16280c6a93ac7f8c5b03e29236aa
monk <2012.monk@gmail.com>
2022-04-11 16:37:14 +0900

    BOJ#15665 N 과 M 11

---
+++ algo-java/src/BOJ/N15665N과M11/Main.java
@@ -0,0 +1,57 @@
+package BOJ.N15665N과M11;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stz;
+    static StringBuffer sb = new StringBuffer();
+    static int n, m;
+    static int N;
+    static int[] numbers, path;
+    static boolean[] element = new boolean[10001];
+
+    public static void main(String[] args) throws IOException {
+        stz = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stz.nextToken());
+        m = Integer.parseInt(stz.nextToken());
+        path = new int[m];
+        numbers = new int[n];
+        stz = new StringTokenizer(br.readLine());
+        N = 0;
+        for (int i = 0; i < n; i++) {
+            int t = Integer.parseInt(stz.nextToken());
+            if (element[t]) {
+                continue;
+            }
+            numbers[N++] = t;
+            element[t] = true;
+        }
+        Arrays.sort(numbers, 0, N);
+        solve(0);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static void solve(int x) {
+        if (x == m) {
+            for (int i : path) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            path[x] = numbers[i];
+            solve(x + 1);
+        }
+    }
+}
