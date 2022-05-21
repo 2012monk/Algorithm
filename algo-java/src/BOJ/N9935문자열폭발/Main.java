69bbcb1e46ca16a0f229209479719964940b4824
monk <2012.monk@gmail.com>
2022-05-21 13:06:47 +0900

    BOJ#9935 문자열 폭발

--- algo-java/src/BOJ/N9935문자열폭발/Main.java
+++ algo-java/src/BOJ/N9935문자열폭발/Main.java
@@ -3,54 +3,37 @@ package BOJ.N9935문자열폭발;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.stream.Collectors;
 
 public class Main {
 
     private static final String EMPTY = "FRULA";
-    static String target;
-    static String s;
+    static char[] target,s, a;
+    static int head = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        s = br.readLine();
-        target = br.readLine();
+        s = br.readLine().toCharArray();
+        target = br.readLine().toCharArray();
+        a = new char[s.length];
         solve();
+        String result = new String(a, 0, head);
+        if (result.equals("")) result = EMPTY;
+        System.out.println(result);
     }
 
     static void solve() {
-        Stack<E> st = new Stack<>();
-        int c = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == target.charAt(c)) {
-                c++;
-            } else if (s.charAt(i) == target.charAt(0)) {
-                c = 1;
-            } else {
-                c = 0;
-            }
-            st.push(new E(s.charAt(i), c));
-            if (c == target.length())  {
-                while (c-- > 0) st.pop();
-                c = 0;
-                if (!st.isEmpty()) {
-                    c = st.peek().p;
-                }
+        for (char c : s) {
+            a[head++] = c;
+            if (match()) {
+                head -= target.length;
             }
         }
-        String res = st.stream().map(e -> String.valueOf(e.c))
-            .collect(Collectors.joining());
-        if (res.isEmpty()) res = EMPTY;
-        System.out.println(res);
     }
 
-    static class E {
-        char c;
-        int p;
-
-        public E(char c, int p) {
-            this.c = c;
-            this.p = p;
+    private static boolean match() {
+        if (head < target.length) return false;
+        for (int i = 0; i < target.length; i++) {
+            if (a[head -target.length + i] != target[i]) return false;
         }
+        return true;
     }
 }
