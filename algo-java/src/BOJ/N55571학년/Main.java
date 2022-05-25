55e45a357347d3ede8d751a6b233a353b86d8080
monk <2012.monk@gmail.com>
2022-05-25 14:06:14 +0900

    BOJ#5557 1 학년

--- algo-java/src/BOJ/N55571학년/Main.java
+++ algo-java/src/BOJ/N55571학년/Main.java
@@ -7,28 +7,27 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int n;
-    static int[] a;
     static long[][] d;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stz;
         n = Integer.parseInt(br.readLine());
-        a = new int[n];
-        d = new long[n][21];
+        d = new long[21][2];
         stz = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(stz.nextToken());
-        }
-        d[0][a[0]] = 1;
+        int start = Integer.parseInt(stz.nextToken());
+        d[start][0] = 1;
 
         for (int i = 1; i < n - 1; i++) {
+            int x = Integer.parseInt(stz.nextToken());
             for (int j = 0; j <= 20; j++) {
-                long v = d[i - 1][j];
+                long v = d[j][(i & 1) ^ 1];
                 if (v == 0) continue;
-                if (j + a[i] <= 20) d[i][j + a[i]] += v;
-                if (j - a[i] >= 0) d[i][j - a[i]] += v;
+                if (j + x <= 20) d[j + x][i&1] += v;
+                if (j - x >= 0) d[j - x][i&1] += v;
+                d[j][(i&1)^1]=0;
             }
         }
-        System.out.println(d[n - 2][a[n - 1]]);
+        int k = Integer.parseInt(stz.nextToken());
+        System.out.println(d[k][n&1]);
     }
 }
