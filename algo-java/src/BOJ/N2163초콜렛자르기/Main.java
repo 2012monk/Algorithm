f9d1edad3f173410d6cfb42a988d30f220bf9e03
monk <2012.monk@gmail.com>
2022-05-28 20:52:58 +0900

    BOJ#2163 초콜렛 자르기

---
+++ algo-java/src/BOJ/N2163초콜렛자르기/Main.java
@@ -0,0 +1,16 @@
+package BOJ.N2163초콜렛자르기;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stz.nextToken());
+        int m = Integer.parseInt(stz.nextToken());
+        System.out.println(n * m - 1);
+    }
+}
